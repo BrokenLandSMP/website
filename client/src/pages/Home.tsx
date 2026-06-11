a5b58081238410f57599063013f99674f997a5bd
HaiHome <HaiHome@yeah.net>
2026-06-12 06:39:11 +0800

    Add AnnouncementBanner to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import AnnouncementBanner from '@/components/AnnouncementBanner';
 import HeroSection from '@/components/HeroSection';
 import FeatureCards from '@/components/FeatureCards';
 import PerspectiveSection from '@/components/PerspectiveSection';
@@ -8,6 +9,9 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <div className="min-h-screen bg-slate-900 text-slate-100">
+      {/* Announcement Banner */}
+      <AnnouncementBanner />
+
       {/* Navigation */}
       <Navbar />
 
